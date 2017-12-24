b1e85def322e01d42b4e5a8b37336e20a28bc49b
Yisroel Meir Blumstein <32102773+RebBlumstein@users.noreply.github.com>
2017-12-24 12:29:12 +0200

    Update testequations.js

--- assets/js/testequations.js
+++ assets/js/testequations.js
@@ -87,7 +87,7 @@ result += "</table>\n";
 
 result += "<br><br>\n";
 
-result += "-25d: " + FindMorningElevationTime(date, latitude, longitude, timezone, -25.0) + "<br>";
+result += "-25.1d: " + FindMorningElevationTime(date, latitude, longitude, timezone, -25.1) + "<br>";
 var sunrise1 = TimeFromFrac(date, AstSunrise(date, latitude, longitude, timezone));
 result += "Sunrise1: " + sunrise1 + "<br>";
 result += "Sunrise2: " + TimeFromFrac(sunrise1, AstSunrise(sunrise1, latitude, longitude, timezone)) + "<br>";
