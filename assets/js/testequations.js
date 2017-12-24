31c7953ebe0f9e2639b418f160fb017b383154ef
Yisroel Meir Blumstein <32102773+RebBlumstein@users.noreply.github.com>
2017-12-24 04:41:22 +0200

    Update testequations.js

--- assets/js/testequations.js
+++ assets/js/testequations.js
@@ -1,3 +1,7 @@
 /*
 This script is designed to test the NOAA Solar Equations for errors and accuracy.
+Writes output to id="TestParagraph"
 */
+
+var date = new Date(); // Get the current Date and Time
+date.setHours(0, 0, 0, 0); // Set the Time to Midnight
