e9760bb289984f098a5064b25fe0efe064a8554b
Yisroel Meir Blumstein <32102773+RebBlumstein@users.noreply.github.com>
2017-12-24 05:02:03 +0200

    Update testequations.js

--- assets/js/testequations.js
+++ assets/js/testequations.js
@@ -7,34 +7,34 @@ var date = new Date(); // Get the current Date and Time
 date.setHours(0, 0, 0, 0); // Set the Time to Midnight
 
 document.getElementById("TestParagraph").value = 
-  "<table><tr>
-    <th>Julian Day</th>
-    <th>Julian Century</th>
-    <th>Geo M Long</th>
-    <th>Geo M Anomaly</th>
-    <th>Ecc Earth Orb</th>
-    <th>Sun Eqn Center</th>
-    <th>Sun True Long</th>
-    <th>Sun True Anom</th>
-    <th>Sun Radius Vect</th>
-    <th>Sun App Long</th>
-    <th>M Obl of Eclipt</th>
-    <th>Obl Corrected</th>
-    <th>Sun RA</th>
-    <th>Sun Dec</th>
-    <th>Var y</th>
-    <th>Eqn of Time</th>
-    <th>HA Sunrise</th>
-    <th>Solar Noon</th>
-    <th>Ast Sunrise</th>
-    <th>Ast Sunset</th>
-    <th>Sunlight Dur</th>
-    <th>Day Fraction</th>
-    <th>True Sol Time</th>
-    <th>Sol HA</th>
-    <th>Sol Zenith</th>
-    <th>Sol Elev</th>
-    <th>Sol Azimu</th>
-    </tr>";
+  "<table><tr>\n"
+    + "<th>Julian Day</th>\n"
+    + "<th>Julian Century</th>\n"
+    + "<th>Geo M Long</th>\n"
+    + "<th>Geo M Anomaly</th>\n"
+    + "<th>Ecc Earth Orb</th>\n"
+    + <th>Sun Eqn Center</th>\n"
+    + <th>Sun True Long</th>\n"
+    + <th>Sun True Anom</th>\n"
+    + <th>Sun Radius Vect</th>\n"
+    + <th>Sun App Long</th>\n"
+    + <th>M Obl of Eclipt</th>\n"
+    + <th>Obl Corrected</th>\n"
+    + <th>Sun RA</th>\n"
+    + <th>Sun Dec</th>\n"
+    + <th>Var y</th>\n"
+    + <th>Eqn of Time</th>\n"
+    + <th>HA Sunrise</th>\n"
+    + <th>Solar Noon</th>\n"
+    + <th>Ast Sunrise</th>\n"
+    + <th>Ast Sunset</th>\n"
+    + <th>Sunlight Dur</th>\n"
+    + <th>Day Fraction</th>\n"
+    + <th>True Sol Time</th>\n"
+    + <th>Sol HA</th>\n"
+    + <th>Sol Zenith</th>\n"
+    + <th>Sol Elev</th>\n"
+    + <th>Sol Azimu</th>\n"
+    + </tr>\n";
 
 document.getElementById("TestParagraph").value += "</table>";
