4b99157195b015219370ac025689c3a79ec547be
Yisroel Meir Blumstein <32102773+RebBlumstein@users.noreply.github.com>
2017-12-25 13:31:08 +0200

    Update calculate.js

--- assets/js/calculate.js
+++ assets/js/calculate.js
@@ -1,4 +1,21 @@
 /* Find the Halachic Zemanim for the given inputs */
 
+function getResults() {
+  var latitude = document.getElementById("locLat").value;
+  var longitude = document.getElementById("locLon").value;
+  var elevation = document.getElementById("locEle").value;
+  var timezone = document.getElementById("locTimeZone").value;
+  var date = document.getElementById("onDate").valueAsDate;
+  
+  var neitz = FindNeitz(date, latitude, longitude, timezone, elevation);
+  var shkiah = FindShkiah(date, latitude, longitude, timezone, elevation);
+  
+  document.getElementById("resultsp").innerHTML = "Neitz is on: " + neitz + "<br>Shkiah is on: " + shkiah
+}
+
+
 // Default the date picker to today
-document.getElementById('onDate').valueAsDate = new Date();
+document.getElementById("onDate").valueAsDate = new Date();
+
+// run getResults on our default inputs
+getResults();
