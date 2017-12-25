ef68026c9b097f5041b14101da5bb81e0e96f67e
Yisroel Meir Blumstein <32102773+RebBlumstein@users.noreply.github.com>
2017-12-25 16:02:38 +0200

    Update halacha.js

--- assets/js/halacha.js
+++ assets/js/halacha.js
@@ -1,13 +1,13 @@
 /* Use the NOAA Solar Equations to calculate relevant Zemanim */
 
 function FindNeitz(date, latitude, longitude, timezone, elevation) {
-  var angle = -SunRadius() - 0*HorizonModifier(elevation) - 0.59;
+  var angle = -SunRadius() - HorizonModifier(elevation) - 0.59;
   
   return FindMorningElevationTime(date, latitude, longitude, timezone, angle);
 }
 
 function FindShkiah(date, latitude, longitude, timezone, elevation) {
-  var angle = -SunRadius() - 0*HorizonModifier(elevation) - 0.59;
+  var angle = -SunRadius() - HorizonModifier(elevation) - 0.59;
   
   return FindEveningElevationTime(date, latitude, longitude, timezone, angle);
 }
