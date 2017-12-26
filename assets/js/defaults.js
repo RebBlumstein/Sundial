a25c46ea14d917c491f9e637656cf65781f65fa1
Yisroel Meir Blumstein <32102773+RebBlumstein@users.noreply.github.com>
2017-12-26 08:26:33 +0200

    Update defaults.js

--- assets/js/defaults.js
+++ assets/js/defaults.js
@@ -5,7 +5,7 @@ document.getElementById("locLon").value = 35.2323083;
 document.getElementById("locEle").value = 842;
 
 // Default the timezone to the computer's local timezone
-document.getElementById("locTimeZone").value = new Date().getTimezoneOffset()/60;
+document.getElementById("locTimeZone").value = -1*(new Date().getTimezoneOffset())/60;
 
 // Default the date picker to today
 document.getElementById("onDate").valueAsDate = new Date();
