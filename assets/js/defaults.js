302122d5e59421bc851a7dd76dfd77a5bfe74a08
Yisroel Meir Blumstein <32102773+RebBlumstein@users.noreply.github.com>
2017-12-26 08:24:05 +0200

    Create defaults.js

---
+++ assets/js/defaults.js
@@ -0,0 +1,11 @@
+
+// Default to Tel Tsiyon, Israel
+document.getElementById("locLat").value = 31.8836667;
+document.getElementById("locLon").value = 35.2323083;
+document.getElementById("locEle").value = 842;
+
+// Default the timezone to the computer's local timezone
+document.getElementById("locTimeZone").value = new Date().getTimezoneOffset()/60;
+
+// Default the date picker to today
+document.getElementById("onDate").valueAsDate = new Date();
